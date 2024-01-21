c55f5cb9ff7c3e500f97ffbdcbff95eb1a85e596
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-21 17:29:06 -0300

    feat: Cria page Home e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+import AppRoutes from './routes'
 
 function App() {
-  return (
-    <>
-      <Header />
-      <section className='container'>
-  <div className='apresentacao'>
-    <p>
-      Olá, sou <br/>
-      <span>Miguel Arcanjo</span> <br/>
-      Dev Full Stack
-    </p>
-    <button className='btn btn-red'>
-      Saiba mais sobre mim
-    </button>
-  </div>
-  <figure>
-    <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
-  </figure>
-</section>
 
-      <Footer />
-    </>
-  );
+  return (
+    <AppRoutes />
+  )
 }
 
-export default App;
+export default App
+
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,20 @@
-import styles from './Header.module.css'
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <span>sidebit.dev</span>
-            <nav>
-                <a href="">Home</a>
-                <a href="">Sobre</a>
-                <a href="">Projetos</a>
-                <a href="">Contatos</a>
-            </nav>
-        </header>
-    )    
+  return (
+    <header className={styles.header}>
+      <Link to="/">
+        <span>sidebit.dev</span>
+      </Link>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/sobre">Sobre</Link>
+        <Link to="/projetos">Projetos</Link>
+        <Link to="/contatos">Contatos</Link>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
