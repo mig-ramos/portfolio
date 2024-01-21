6777abc90235e0aa9be0869977e15b7f08438c8a
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-21 16:16:33 -0300

    feat: Cria os componentes Footer, Header e Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 function App() {
-
   return (
     <>
-      <h1>Olá mundo!</h1>
+      <Header />
+      <section className='container'>
+  <div className='apresentacao'>
+    <p>
+      Olá, sou <br/>
+      <span>Miguel Arcanjo</span> <br/>
+      Dev Full Stack
+    </p>
+    <button className='btn btn-red'>
+      Saiba mais sobre mim
+    </button>
+  </div>
+  <figure>
+    <img className='img-home' src="/developer-red.svg" alt="Imagem de Home" />
+  </figure>
+</section>
+
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com ❣️ por Miguel Arcanjo © 2023
+        </footer>
+    )
+}
+
+export default Footer
