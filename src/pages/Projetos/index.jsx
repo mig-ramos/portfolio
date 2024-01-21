e30c6e9b4a4ffab03d056713b957f05dde20ab96
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-21 19:39:53 -0300

    feat: Cria Card da page Projetos

---
+++ src/pages/Projetos/index.jsx
@@ -0,0 +1,15 @@
+
+import Card from '../../components/Card'
+
+function Projetos() {
+    return (
+        <>
+            <h2>Projetos</h2>
+            <Card />
+            <Card />
+            <Card />
+        </>
+    )
+}
+
+export default Projetos
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
-// import Projetos from './pages/Projetos'
+import Projetos from './pages/Projetos'
 import Contatos from './pages/Contatos'
 import Page404 from './pages/Page404'
 import PageBase from './pages/PageBase'
@@ -13,7 +13,7 @@ function AppRoutes() {
                 <Route path='/' element={ <PageBase /> }>
                     <Route index element={ <Home /> }></Route>
                     <Route path="/sobre" element={ <Sobre /> }></Route>
-                    {/* <Route path="/projetos" element={ <Projetos /> }></Route> */}
+                    <Route path="/projetos" element={ <Projetos /> }></Route>
                     <Route path="/contatos" element={ <Contatos /> }></Route>
                     <Route path="*" element={ <Page404 /> }></Route>
                 </Route>
