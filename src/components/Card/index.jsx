bac99ba2a068ef9d951d1169e42c910dd054d641
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-01-21 20:01:12 -0300

    feat: Consome API do Github

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,26 +1,35 @@
+import styles from "./Card.module.css";
+import { FaHtml5, FaCss3Alt, FaJs, FaReact } from "react-icons/fa";
+import { BsArrowRight } from "react-icons/bs";
+// import { Link } from "react-router-dom";
 
-import styles from './Card.module.css'
-import { FaHtml5, FaCss3Alt, FaJs, FaReact } from 'react-icons/fa'
-import { BsArrowRight } from 'react-icons/bs'
-
-function Card() {
-    return (
-        <section className={styles.card}>
-            <h3>Título do projeto</h3>
-            <p>Texto descritivo do projeto.</p>
-            <div className={styles.card_footer}>
-                <div className={styles.card_icones}>
-                    <FaHtml5 />
-                    <FaCss3Alt />
-                    <FaJs />
-                    <FaReact />
-                </div>
-                <button className={styles.botao}>
+// eslint-disable-next-line react/prop-types
+function Card({ name, description, html_url }) {
+  return (
+    <section className={styles.card}>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <div className={styles.card_footer}>
+        <div className={styles.card_icones}>
+          <FaHtml5 />
+          <FaCss3Alt />
+          <FaJs />
+          <FaReact />
+        </div>
+        {/* <Link to={html_url} className={styles.botao}>
                     <BsArrowRight />
-                </button>
-            </div>
-        </section>
-    )
+                </Link> */}
+        <a
+          href={html_url}
+          className={styles.botao}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <BsArrowRight />
+        </a>
+      </div>
+    </section>
+  );
 }
 
-export default Card
+export default Card;
